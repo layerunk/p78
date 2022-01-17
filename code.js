8e966fd10ca1915bbdfeb21de0102cafc09011ba
layerunk <89783661+layerunk@users.noreply.github.com>
2022-01-17 17:10:16 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,7 @@
+var updatedimages = ["mercury.jpeg" , "suntransparent.jpeg"]
+var updatednamess = ["father" , "son"]
+
+function next() {
+ document.getElementById("im").innerHTML = updatedimages;
+ document.getElementById("name").innerHTML = updatednamess;
+}
